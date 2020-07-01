7bd87d58ff7f435f8f7538a8933c2da761eef37f
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-07-01 18:00:11 -0300

    feat(backend): configuração para teste e desenvolvimento

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,8 +1,15 @@
+/* eslint-disable */
+process.env.NODE_ENV &&
+  process.env.NODE_ENV !== 'prod' &&
+    require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` });
+/* eslint-enable */
+
 module.exports = {
-  username: 'postgres',
-  password: 'postgres',
-  database: 'cm_db',
-  host: 'localhost',
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   dialect: 'postgres',
   define: {
     timestamps: true,
