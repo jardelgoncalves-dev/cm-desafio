adbe15849cce85fe5951e141aafb919e1acba163
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-07-01 15:16:24 -0300

    feat(backend): configurações do banco

---
+++ backend/src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  username: 'postgres',
+  password: 'postgres',
+  database: 'cm_db',
+  host: 'localhost',
+  dialect: 'postgres',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
