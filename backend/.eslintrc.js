45c6ae0c21cfe520ba6fc5e22d5961ffe8f494b3
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-07-01 15:16:04 -0300

    feat(backend): setup inicial

---
+++ backend/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    es2020: true,
+    node: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-underscore-dangle': 'off',
+    'import/prefer-default-export': 'off',
+    'func-names': 'off',
+    'consistent-return': 'off',
+    'prettier/prettier': 'error',
+    'class-methods-use-this': 'off',
+    'no-param-reassign': 'off',
+    camelcase: 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+  },
+  overrides: [
+    {
+      files: ['src/app/database/migrations/*.js'],
+      rules: {
+        'no-unused-vars': 'off',
+      },
+    },
+  ],
+};
---
+++ backend/jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  bail: true,
+  clearMocks: true,
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.test.js?(x)'],
+};
